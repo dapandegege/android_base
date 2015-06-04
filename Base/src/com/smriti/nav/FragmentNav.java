c1cd8c30e922ad32cc3068e8a0da6b51eb0e048d
wangjufan <wangjufan@126.com>
2015-06-04 20:09:11 +0800

    FragmentNav
    FragmentNav for pop commit !

--- Base/src/com/smriti/nav/FragmentNav.java
+++ Base/src/com/smriti/nav/FragmentNav.java
@@ -1,6 +1,9 @@
 package com.smriti.nav;
 
 
+import java.util.Iterator;
+import java.util.Stack;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,59 +12,113 @@ import android.support.v4.app.FragmentTransaction;
 
 public class FragmentNav extends Nav {
 	
+	public enum FragmentPopType{
+		FragmentPopType_Exclusive(0),FragmentPopType_Inclusive(1);
+		private int value = 0;
+	    private FragmentPopType(int value) {   
+	        this.value = value;
+	    }
+	    public static FragmentPopType valueOf(int value) {    
+	        switch (value) {
+	        case 0:
+	            return FragmentPopType_Exclusive;
+	        case 1:
+	            return FragmentPopType_Inclusive;
+	        default:
+	            return null;
+	        }
+	    }
+	    public int value() {
+	        return this.value;
+	    }
+	}
+	
+	private Stack<String> stackParas;
+	private FragmentActivity activity = null;
+	private int containerViewId = 0;
+	
 	private static FragmentNav fragmentNav = new FragmentNav();
 	public static FragmentNav defaultInstance() {
 		return fragmentNav;
 	}
 	
-	private FragmentActivity activity = null;
-	private int rootLayoutID = 0;
 	
+	public FragmentNav(){
+		stackParas = new Stack<String>();
+	}
+	
+	private FragmentManager getFragmentManager(){
+		return activity.getSupportFragmentManager();
+	}
+	
+	/*
+	 * getters and setters
+	 */
 	public void setActivity(FragmentActivity activity){
 		this.activity = activity;
 	}
 	public FragmentActivity getActivity(){
 		return activity;
 	}
-	public int getRootLayoutID() {
-		return rootLayoutID;
+	
+	public int getcontainerViewId() {
+		return containerViewId;
 	}
-	public void setRootLayoutID(int rootLayoutID) {
-		this.rootLayoutID = rootLayoutID;
+	public void setcontainerViewId(int containerViewId) {
+		this.containerViewId = containerViewId;
 	}
 	
-	
-	public FragmentManager getFragmentManager(){
-		return activity.getSupportFragmentManager();
-	}
-
+	/*
+	 * pop and push
+	 */
 	public void replaceFragment(Fragment target, Bundle bundle, TransitType type) {
 		if (bundle != null) {
 			target.setArguments(bundle);
 		}
 		FragmentTransaction transaction = getFragmentManager().beginTransaction();
 		transaction.setTransition(type.get());
-		System.out.println("replaceFragment class name == "+target.getClass().getName());
 		String tag = target.getClass().getName();
-		transaction.replace(rootLayoutID, target,tag);
+		transaction.replace(containerViewId, target,tag);
 		transaction.commit();
 	}
+	
 	public void pushFragment(Fragment target, Bundle bundle, TransitType type) {
 		if (bundle != null) {
 			target.setArguments(bundle);
 		}
+		
 		FragmentTransaction transaction = getFragmentManager().beginTransaction();
+		
 		transaction.setTransition(type.get());
-		System.out.println("pushFragment class name == "+target.getClass().getName());
 		String tag = target.getClass().getName();
-		transaction.replace(rootLayoutID, target,tag);
+		transaction.replace(containerViewId, target,tag);
 		transaction.addToBackStack(tag);
 		transaction.commit();
+		
+		stackParas.push(tag);
 	}
 	
 	public void popFragment() {
 		getFragmentManager().popBackStack();
 	}
-	
+	public void popFragmentContain(String tag){
+		popFragment(tag, FragmentPopType.FragmentPopType_Inclusive);
+	}
+	public void popFragmentOnTop(String tag){
+		popFragment(tag, FragmentPopType.FragmentPopType_Exclusive);
+	}
+	public void popFragment(String tag, FragmentPopType popType){
+		if (tag == null) {
+			return;
+		}
+		for (Iterator<String> iterator = stackParas.iterator(); iterator.hasNext();) {
+			String ltag = (String) iterator.next();
+			if (ltag.equals(tag)) {
+				getFragmentManager().popBackStack(tag, popType.value);
+				 stackParas.pop();
+				break;
+			}
+		}
+	}
 	
 }
